584fd371d88d80a7329daae77b8aff66a873f462
Judicael <djmx972@gmail.com>
2020-08-20 23:48:08 +0200

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: true,
+        left: false,
+        right: false,
+        bottom: true,
+        minimum: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.05,
+            0, MediaQuery.of(context).size.width * 0.05, 0),
+        child: _mainCollum(),
+      ),
+    );
+  }
+
+  Widget _mainCollum() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.menu,
+              color: Colors.black87,
+              size: 35,
+            ),
+            Container(
+              height: 39,
+              width: 39,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/images/logo.jpg'),
+                ),
+              ),
+            ),
+            Icon(
+              Icons.search,
+              color: Colors.black87,
+              size: 35,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
